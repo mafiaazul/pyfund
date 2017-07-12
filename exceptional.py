c361d61bfb15c7407b9fe030ac7e083cd8280c4f
Rocha, Rodrigo <Rocha.Rodrigo@principal.com>
2017-07-12 08:10:30 -0500

    1st commit

--- exceptional.py
+++ exceptional.py
@@ -1,6 +1,9 @@
-'''A module for demonstrating exceptions.'''
+"""A module for demonstrating exceptions."""
 
 def convert(s):
-    '''Convert to an integer'''
-    x = int(s)
-    returnt x
+    """Convert to an integer"""
+    x = -1
+    try:
+        x = int(s)
+    except (ValueError, TypeError):
+    return x
